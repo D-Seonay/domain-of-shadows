9dc64ec884c46577fb165712a820fdd5632f7408
Mathéo DELAUNAY <dseonay@gmail.com>
2026-05-01 04:37:37 +0200

    chore: initial project scaffolding

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-black italic text-zinc-100">
+        DOMAIN OF SHADOWS
+      </h1>
+    </div>
+  )
+}
